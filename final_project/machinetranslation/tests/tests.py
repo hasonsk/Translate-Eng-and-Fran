6da9fe2bdc5b2b093a9a1424e48333e12cd0ba90
hasonsk <sonhh2003@gmail.com>
2023-07-20 00:06:23 +0700

    update tests.py

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,13 +1,16 @@
 import unittest 
+import sys
+sys.path.append("..")  
 
 from translator import english_to_french, french_to_english
 
-class TestEtoF(unittest.TestCase):
+class Test(unittest.TestCase):
     def test1(self):
         self.assertEqual(english_to_french("Hello"), "Bonjour")
-
-class TestFtoE(unittest.TestCase):
+        self.assertNotEqual(english_to_french("Hello"), "Salut")
     def test2(self):
         self.assertEqual(french_to_english("Bonjour"), "Hello")
+        self.assertNotEqual(french_to_english("bien"), "bad")
+
 
 unittest.main()
